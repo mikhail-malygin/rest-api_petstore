2d9cbbec2493680b100af5378cfa4cdb074a59a3
Mikhail Malygin <mihail.malygin@gmail.com>
2022-10-18 13:38:28 +0500

    Added lombok model

---
+++ src/test/java/models/lombok/ResponseLombokModel.java
@@ -0,0 +1,10 @@
+package models.lombok;
+
+import lombok.Data;
+
+@Data
+public class ResponseLombokModel {
+
+    private String id;
+    private String name;
+}
--- src/test/java/petstore/petStoreTests.java
+++ src/test/java/petstore/petStoreTests.java
@@ -1,24 +1,39 @@
 package petstore;
 
+import models.lombok.BodyLombokModel;
+import models.lombok.ResponseLombokModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class petStoreTests extends TestBase {
 
     @Test
-    void getPetById() {
+    @DisplayName("Get pet information by id")
+    void getPetByIdTests() {
 
-        given()
+        BodyLombokModel bodyLombokModel = new BodyLombokModel();
+        bodyLombokModel.setId("10");
+        bodyLombokModel.setName("doggie");
+
+        ResponseLombokModel response = given()
                 .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(bodyLombokModel)
                 .when()
-                .get("/10")
+                .get("/" + bodyLombokModel.getId())
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("id", is(10))
-                .body("name", is("doggie"));
+                .extract()
+                .as(ResponseLombokModel.class);
+
+        assertThat(response.getId()).isEqualTo("10");
+        assertThat(response.getName()).isEqualTo("doggie");
     }
 }
